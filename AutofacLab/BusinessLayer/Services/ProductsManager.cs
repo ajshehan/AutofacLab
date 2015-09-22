a902c9baed33939849b8c62257b61bf471616745
Shehan Pathmaperuma <aj.shehan@gmail.com>
2015-09-22 23:05:26 +0530

    Added solution

---
+++ AutofacLab/BusinessLayer/Services/ProductsManager.cs
@@ -0,0 +1,18 @@
+﻿using BusinessLayer.ViewModels;
+using System.Collections.Generic;
+
+namespace BusinessLayer.Services
+{
+    public class ProductsManager
+    {
+        public List<ProductsViewModel> GetAllProducts()
+        {
+            List<ProductsViewModel> productsList = new List<ProductsViewModel>();
+
+
+
+            return productsList;
+        }
+
+    }
+}
