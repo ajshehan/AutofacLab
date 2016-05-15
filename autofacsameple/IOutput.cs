fa8dc42d6410225b39ff5ca0138e9b191215b823
ajshehan <aj.shehan@gmail.com>
2016-05-15 19:02:15 +0530

    Initial commit of project

---
+++ autofacsameple/IOutput.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DemoWriter
+{
+    public interface IOutput
+    {
+        void Write(string content);
+    }
+
+    public class ConsoleOutput : IOutput
+    {
+        public void Write(string content)
+        {
+            Console.WriteLine(content);
+        }
+    }
+
+
+    public interface IDateWriter
+    {
+        void WriteDate();
+    }
+
+    public interface ITomorrowDateWriter
+    {
+        void WriteDate();
+    }
+
+    public class TodayWriter : IDateWriter
+    {
+        private IOutput _output;
+
+        public TodayWriter(IOutput output)
+        {
+            this._output = output;
+        }
+
+        public void WriteDate()
+        {
+            this._output.Write(DateTime.Today.ToShortDateString());
+        }
+    }
+
+    public class TomorroWriter : ITomorrowDateWriter
+    {
+        private IOutput _output;
+
+        public TomorroWriter(IOutput output)
+        {
+            this._output = output;
+        }
+
+        public void WriteDate()
+        {
+            this._output.Write(DateTime.Today.AddDays(1).ToShortDateString());
+        }
+    }
+}
---
+++ autofacsameple/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Autofac;
+using DemoWriter;
+
+namespace autofacsameple
+{
+    class Program
+    {
+
+        private static IContainer _container { get; set; }
+
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<ConsoleOutput>().As<IOutput>();
+            builder.RegisterType<TodayWriter>().As<IDateWriter>().UsingConstructor(typeof(IOutput));
+            builder.RegisterType<TomorroWriter>().As<ITomorrowDateWriter>();
+            _container = builder.Build();
+
+            WriteDate();
+
+            Console.ReadLine();
+        }
+
+        public static void WriteDate()
+        {
+            using (var scope = _container.BeginLifetimeScope())
+            {
+                var writer = scope.Resolve<IDateWriter>();
+                writer.WriteDate();                
+            }
+        }
+    }
+}
